5f49b3fdcf3cdb442fd4e38b888491ce16f9df27
Никита Ковалдов <49562847+N3kitGame@users.noreply.github.com>
2023-02-19 02:51:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -20,34 +20,32 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t p = 0, l = 0;
-	while (n > 0) {
-		if (checkPrime(p)) {
-			l = p;
-			--n;
-		}
-		++p;
-	}
-	return l;
+  uint64_t p = 0, l = 0;
+  while (n > 0) {
+    if (checkPrime(p)) {
+      l = p;
+      --n;
+    }
+    ++p;
+  }
+  return l;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t k = 1, p = value + 1;
-	while (k > 0) {
-    if (checkPrime(p)) {
-			k = 0;
-			return p;
-		}
-		p++;
-	}
+  while (true) {
+    value++;
+    if (checkPrime(value)) {
+      return value;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t k = 0;
-	for (int i = 1; i < hbound; i++) {
-		if (checkPrime(i)) {
-			k = k + i;
-		}
-	}
-	return k;
+  for (int i = 1; i < hbound; i++) {
+    if (checkPrime(i)) {
+      k = k + i;
+    }
+  }
+  return k;
 }
